beb8c19620a2fdee5e8f75df262913e23f083c78
Andrew Kolumbic <akolumbic@nexient.com>
2020-10-28 18:34:34 -0400

    fin

--- photo-gallery/src/app/tab2/tab2.page.ts
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
-import { PhotoService } from '../services/photo.service'
+import { ActionSheetController } from '@ionic/angular'
+import { PhotoService, Photo } from '../services/photo.service'
 
 
 @Component({
@@ -9,7 +10,10 @@ import { PhotoService } from '../services/photo.service'
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+  ) { }
 
   // tslint:disable-next-line: use-lifecycle-interface
   async ngOnInit() {
@@ -18,6 +22,28 @@ export class Tab2Page {
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery()
+  }
+
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position)
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    })
+    await actionSheet.present()
   }
 
 }
