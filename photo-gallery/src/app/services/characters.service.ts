71dfab15e9b7ccb1cf2d4418fc6e86e0dc58f152
Andrew Kolumbic <akolumbic@nexient.com>
2020-10-28 23:34:30 -0400

    couple of assets

--- photo-gallery/src/app/services/characters.service.ts
+++ photo-gallery/src/app/services/characters.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core'
 
+import characters from '../../assets/character-list'
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class CharactersService {
+  characters = characters
 
   constructor() { }
 }
---
+++ photo-gallery/src/assets/character-list.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: 'Mace Corxian',
+    level: 2,
+    race: 'Half-Elf',
+    class: 'Ranger'
+  },
+  {
+    name: 'Seebo Beren',
+    level: 2,
+    race: 'Gnome',
+    class: 'Artificer'
+  },
+  {
+    name: 'Gogi',
+    level: 2,
+    race: 'Tortle',
+    class: 'Druid'
+  },
+  {
+    name: 'Skaxes',
+    level: 2,
+    race: 'Genasi',
+    class: 'Monk'
+  }
+] as Falstera.Character[]
