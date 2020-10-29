466094639da8a839abedf77087ca4ead11b6a94d
Andrew Kolumbic <akolumbic@nexient.com>
2020-10-29 01:16:21 -0400

    comment out dnd service

---
+++ falstera/src/app/dnd-search/search.ts
@@ -0,0 +1,21 @@
+// import { DnDService, Path } from '../services/dnd.service'
+// import { Component, OnInit } from '@angular/core'
+
+// @Component({
+//   selector: 'app-dnd-search',
+//   templateUrl: './search.html',
+//   styleUrls: ['./search.scss']
+// })
+
+// export class DnDSearchComponent implements OnInit {
+//   results: Promise<any>
+//   input: string
+//   searchType: Path
+
+//   constructor(private search: DnDService) {}
+//   ngOnInit() {}
+
+//   onSearch() {
+//     this.results = this.search.searchFor(this.searchType, this.input)
+//   }
+// }
--- falstera/src/app/services/dnd.service.ts
+++ falstera/src/app/services/dnd.service.ts
@@ -1,27 +1,27 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+// import { Injectable } from '@angular/core'
+// import { HttpClient } from '@angular/common/http'
 
-enum Path {
-  monsters = 'monsters/',
-  spells = 'spells/',
-  equipment = 'equipment/'
-}
+// export enum Path {
+//   monsters = 'monsters/',
+//   spells = 'spells/',
+//   equipment = 'equipment/'
+// }
 
-@Injectable({
-  providedIn: 'root',
-})
+// @Injectable({
+//   providedIn: 'root',
+// })
 
-export class DnDService {
-  URL = 'https://www.dnd5eapi.co/api/'
+// export class DnDService {
+//   URL = 'https://www.dnd5eapi.co/api/'
 
-  constructor(private http: HttpClient) {}
+//   constructor(private http: HttpClient) {}
 
-  getAll(type: Path) {
-    return this.http.get(`${URL}${Path[type]}`)
-  }
+//   getAll(type: Path) {
+//     return this.http.get(`${this.URL}${Path[type]}`).toPromise()
+//   }
 
-  searchFor(type: Path, input: string) {
-    const query = input.split(' ').map(word => word.toLowerCase()).join('-')
-    return this.http.get(`${URL}${Path[type]}${query}`)
-  }
-}
+//   searchFor(searchType: Path, input: string) {
+//     const query = input.split(' ').map(word => word.toLowerCase()).join('-')
+//     return this.http.get(`${this.URL}${Path[searchType]}${query}`).toPromise()
+//   }
+// }
