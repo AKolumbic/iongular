6ae1c443c7b75d8f8204becbea374ddbc56353bc
Andrew Kolumbic <akolumbic@nexient.com>
2020-10-29 23:45:08 -0400

    location list

---
+++ falstera/src/assets/locations.list.ts
@@ -0,0 +1,181 @@
+export const locations: Falstera.Location[] = [
+  {
+    name: 'Magma Bay',
+    type: 'Region',
+    inhabitants: ['Dwarf', 'Human', 'Goliath', 'Gnome']
+  },
+  {
+    name: 'Magheim',
+    type: 'City',
+    inhabitants: ['Dwarf'],
+  },
+  {
+    name: 'Palebark Village',
+    type: 'Town',
+    inhabitants: ['Dwarf', 'Human'],
+  },
+  {
+    name: 'Fortenheim',
+    type: 'Region',
+    inhabitants: ['Dwarf', 'Duregar', 'Human']
+  },
+  {
+    name: `Faer's Landing`,
+    type: 'Town',
+    inhabitants: ['Dwarf']
+  },
+  {
+    name: `Foren's Keep`,
+    type: 'Landmark',
+    inhabitants: ['Dwarf'],
+    dmNotes: 'Secret Vedalken Ruins'
+  },
+  {
+    name: 'Gnomehole Village',
+    type: 'Town',
+    inhabitants: ['Gnome'],
+  },
+  {
+    name: 'Ironkeep',
+    type: 'City',
+    inhabitants: ['Dwarf', 'Gnome', 'Human'],
+  },
+  {
+    name: 'Nothren Vale',
+    type: 'Region',
+    inhabitants: ['Dwarf', 'Gnome', 'Human', 'Goliath'],
+  },
+  {
+    name: 'Draconia',
+    type: 'City',
+    inhabitants: ['Human', 'Dragonborn', 'Elf', 'Half-Elf', 'Dwarf', 'Gnome'],
+  },
+  {
+    name: 'Drakon Coast',
+    type:  'Region',
+    inhabitants: ['Human']
+  },
+  {
+    name: `Lion's Town`,
+    type: 'Town',
+    inhabitants: ['Human', 'Leonin', 'Loxodon', 'Satyr']
+  },
+  {
+    name: 'Mount Leotonia',
+    type: 'Region',
+    inhabitants: ['Leonin', 'Centaur', 'Minotaur', 'Satyr', 'Loxodon'],
+  },
+  {
+    name: 'Seagate Village',
+    type: 'Town',
+    inhabitants: ['Human', 'Halfling']
+  },
+  {
+    name: `Dragon's Port`,
+    type: 'City',
+    inhabitants: ['Human', 'Dwarf', 'Hobgoblin', 'Goblin', 'Triton']
+  },
+  {
+    name: 'Korsyre Isles',
+    type: 'Region',
+    inhabitants: ['Triton', 'Tabaxi', 'Lizardfolk', 'Tortle']
+  },
+  {
+    name: 'Kulboran Peninsula',
+    type: 'Region',
+    inhabitants: ['Orc', 'Hobgoblin', 'Goblin', 'Kobold'],
+    dmNotes: 'Kulboran Horde'
+  },
+  {
+    name: 'Orkmont',
+    type: 'City',
+    inhabitants: ['Orc', 'Hobgoblin', 'Goblin', 'Goliath']
+  },
+  {
+    name: 'Ruins of Kulboras',
+    type: 'Landmark',
+    inhabitants: ['Hobgoblin', 'Goblin']
+  },
+  {
+    name: 'Dreadwater Cove',
+    type: 'Landmark',
+    inhabitants: ['Hobgoblin', 'Goliath']
+  },
+  {
+    name: 'Hellspire Tower',
+    type: 'Landmark',
+    inhabitants: ['Kobold']
+  },
+  {
+    name: 'Baleston',
+    type: 'City',
+    inhabitants: ['Human', 'Halfling', 'Shifter']
+  },
+  {
+    name: 'Lake Alenor',
+    type: 'Region',
+    inhabitants: ['Halfling', 'Shifter', 'Firbolg']
+  },
+  {
+    name: 'Kortham',
+    type: 'City',
+    inhabitants: ['Human', 'Half-Elf', 'Halfling', 'Shifter', 'Changling', 'Goblin', 'Tiefling'],
+    dmNotes: 'Known Vampire Population'
+  },
+  {
+    name: 'Sentrelian Forest',
+    type: 'Region',
+    inhabitants: ['Firbolg', 'Shifter']
+  },
+  {
+    name: `Alte'Petl`,
+    type: 'City',
+    inhabitants: ['Human', 'Elf', 'Half-Elf', 'Aasimiar', 'Tiefling', 'Genasi']
+  },
+  {
+    name: 'Crossroads',
+    type: 'Town',
+    inhabitants: ['Human', 'Elf']
+  },
+  {
+    name: `Gwyn'bor`,
+    type: 'City',
+    inhabitants: ['Elf'],
+    dmNotes: 'Ravnican Veldalken Refugees'
+  },
+  {
+    name: 'Eastgate Academy',
+    type: 'Landmark',
+    inhabitants: ['Human', 'Elf', 'Half-Elf', 'Gnome', 'Dwarf']
+  },
+  {
+    name: 'Dethvale',
+    type: 'Town',
+    inhabitants: ['Vampire']
+  },
+  {
+    name: 'Oasis',
+    type: 'City',
+    inhabitants: ['Human', 'Genasi', 'Tiefling'],
+    dmNotes: 'Secret Gith vs Illithid Cold War'
+  },
+  {
+    name: 'Sarnathi Desert',
+    type: 'Region',
+    inhabitants: ['Genasi', 'Gith', 'Illithid']
+  },
+  {
+    name: 'Ruins of Sarnath',
+    type: 'Landmark',
+    inhabitants: ['Illithid', 'Human', 'Orc', 'Hobgoblin', 'Tiefling'],
+    dmNotes: 'Cult of Vertithia'
+  }
+]
+
+export const realms: Falstera.Realm[] = [
+  'Falstera',
+  'Faerûn',
+  'Eberron',
+  'Ravnica',
+  'Ravenloft'
+]
--- falstera/src/types.d.ts
+++ falstera/src/types.d.ts
@@ -1,53 +1,89 @@
 declare namespace Falstera {
-  
-
   interface Character {
     name: string;
     level: number;
-    race: Race;
+    race: PlayableRace;
     class: CharacterClass;
   }
 
-  type Race =
-  | "Dwarf"
-  | "Elf"
-  | "Genasi"
-  | "Gnome"
-  | "Goblin"
-  | "Half-Elf"
-  | "Halfling"
-  | "Human"
-  | "Orc"
-  | "Tortle"
-
-  type CharacterClass =
-  | "Artificer"
-  | "Barbarian"
-  | "Bard"
-  | "Cleric"
-  | "Druid"
-  | "Fighter"
-  | "Monk"
-  | "Paladin"
-  | "Ranger"
-  | "Rogue"
-  | "Sorcerer"
-  | "Warlock"
-  | "Wizard"
-
-  interface Location {
-    name: string
-    description: string
-    population: Race[]
+  interface Race {
+    name: Humanoid;
+    description: string;
+    nativeRealm: Realm | Realm[];
+    traits?: string[];
   }
 
+  type Realm = "Falstera" | "Faerûn" | "Eberron" | "Ravnica" | "Ravenloft";
+
+  type Humanoid =
+    | PlayableRace
+    | "Dragonborn"
+    | "Duregar"
+    | "Tiefling"
+    | "Orc"
+    | "Leonin"
+    | "Satyr"
+    | "Aasimiar"
+    | "Firbolg"
+    | "Hobgoblin"
+    | "Kobold"
+    | "Lizardfolk"
+    | "Tabaxi"
+    | "Triton"
+    | "Centaur"
+    | "Loxodon"
+    | "Minotaur"
+    | "Vedalken"
+    | "Changling"
+    | "Shifter"
+    | "Gith"
+    | "Vampire"
+    | "Illithid";
+
+  type PlayableRace =
+    | "Dwarf"
+    | "Elf"
+    | "Genasi"
+    | "Gnome"
+    | "Goblin"
+    | "Goliath"
+    | "Half-Elf"
+    | "Halfling"
+    | "Human"
+    | "Tortle";
+
+  type CharacterClass =
+    | "Artificer"
+    | "Barbarian"
+    | "Bard"
+    | "Cleric"
+    | "Druid"
+    | "Fighter"
+    | "Monk"
+    | "Paladin"
+    | "Ranger"
+    | "Rogue"
+    | "Sorcerer"
+    | "Warlock"
+    | "Wizard";
+
   interface NPC {
-    name: string
-    race: Race
-    combatRating: '1/8' | '1/4' | '1/2' | number
-    monsterType: string
+    name: string;
+    type: "npc" | "monster";
+    race: Humanoid;
+    combatRating?: "1/8" | "1/4" | "1/2" | number;
+    monsterType?: string;
+    nativeRealm?: Realm | Realm[];
+  }
+
+  interface Location {
+    name: string;
+    type: "City" | "Town" | "Region" | "Landmark";
+    inhabitants: Humanoid[];
+    description?: string;
+    dmNotes?: string;
   }
 }
 
-export = Falstera
-export as namespace Falstera
+export = Falstera;
+export as namespace Falstera;
